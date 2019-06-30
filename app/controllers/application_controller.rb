3faa2814f5931ef05166440ada8a95d1aa964e9d
Mario Adamo Jr <marioadamojr@gmail.com>
2019-06-30 16:42:28 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,28 @@ class ApplicationController < Sinatra::Base
 
   # code actions here!
 
+  get '/' do
+    redirect '/recipes'
+  end
+
+  get '/recipes/new' do
+    erb :new
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.create(name: params[:name],ingredients: params[:ingredients], cook_time: params[:cook_time])
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    binding.pry
+    erb :show
+  end
+
 end
