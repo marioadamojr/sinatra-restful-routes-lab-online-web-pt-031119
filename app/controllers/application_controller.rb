7f4a84f4adb4018012fdb40262400b80c5c20697
Mario Adamo Jr <marioadamojr@gmail.com>
2019-06-30 16:43:50 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,8 +26,20 @@ class ApplicationController < Sinatra::Base
 
   get '/recipes/:id' do
     @recipe = Recipe.find_by_id(params[:id])
-    binding.pry
     erb :show
+  end
+
+  get '/articles/:id/edit' do  #load edit form
+    @article = Article.find_by_id(params[:id])
+    erb :edit
+    end
+
+  patch '/articles/:id' do #edit action
+    @article = Article.find_by_id(params[:id])
+    @article.title = params[:title]
+    @article.content = params[:content]
+    @article.save
+    redirect to "/articles/#{@article.id}"
   end
 
 end
